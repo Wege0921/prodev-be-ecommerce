fff85cda10423a16fbb7a15a58983c7d522a875b
Wege0921 <wegemuluneh@gmail.com>
2025-09-28 12:09:06 +0300

    nginix.config and setting.py configured

--- products/models.py
+++ products/models.py
@@ -47,13 +47,10 @@ class Order(models.Model):
         default="PENDING"
     )
 
-
-
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    #price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.quantity} x {self.product.title} (Order {self.order.id})"
